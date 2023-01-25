f3cb9824f96f5fa39a6326669ae3500fb868a8d0
alekseev_a_practik <rubus_alekseev_a_pracktick@mail.ru>
2023-01-25 12:07:41 +0300

    refactoring files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include(('polls.urls', 'polls'), namespace="polls")),
     # not 'include('polls.urls', namespace="polls")'
+    path('polls/', include(('polls.urls', 'polls'), namespace="polls")),
     path('admin/', admin.site.urls),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,3 @@
-# Create your views here.
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -6,21 +5,6 @@ from django.views import generic
 
 from .models import Question, Choice
 
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
-#
-#
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-#
-#
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
