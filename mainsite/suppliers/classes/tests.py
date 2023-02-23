247f3bf21bad45167fe32d42e9fadab7f82fcfe1
alekseev_a_practik <rubus_alekseev_a_pracktick@mail.ru>
2023-02-24 01:03:32 +0300

    add tests

--- mainsite/suppliers/classes/tests.py
+++ mainsite/suppliers/classes/tests.py
@@ -21,4 +21,14 @@ class NodeTests(TestCase):
         self.assertEqual(main_node.name, 'Материал DKC')
         self.assertEqual(main_node.has_children(), True)
         self.assertEqual(main_node.has_parent(), False)
-        # self.assertEqual(str(self.test_supplier), f'{self.name} ({str(self.is_outdated)[0]})')
+        self.assertEqual(main_node.parent, None)
+        self.assertEqual(main_node.children, [
+            Node('general_params', 'Материал'),
+            Node('material_certificates', 'Сертификаты материала'),
+            Node('stock', 'Остатки на складах'),
+        ])
+
+        self.assertEqual(main_node.find_child_node_by_name('123'), None)
+        self.assertEqual(main_node.find_child_node_by_number('123'), None)
+        self.assertNotEqual(main_node.find_child_node_by_name('Материал'), None)
+        self.assertNotEqual(main_node.find_child_node_by_number('general_params'), None)
