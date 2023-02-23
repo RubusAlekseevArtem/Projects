fdf8b0be0dc11a96b5c1be734e46583024f012a9
alekseev_a_practik <rubus_alekseev_a_pracktick@mail.ru>
2023-02-24 00:42:26 +0300

    create test for model

--- mainsite/suppliers/tests.py
+++ mainsite/suppliers/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import Supplier, SupplierParameter
 
-# Create your tests here.
+
+class SupplierModelTests(TestCase):
+    name = 'name'
+    is_outdated = False
+    test_supplier: Supplier = None
+
+    def setUp(self) -> None:
+        self.test_supplier = Supplier.objects.create(name=self.name,
+                                                     is_outdated=self.is_outdated)
+
+    def tearDown(self) -> None:
+        self.test_supplier.delete()
+
+    def test_creation(self):
+        self.assertEqual(str(self.test_supplier), f'{self.name} ({str(self.is_outdated)[0]})')
+
+
+class SupplierParameterModelTests(TestCase):
+    name = 'name'
+    parameter_name = 'parameter_name'
+    is_outdated = False
+    test_supplier: Supplier
+    test_supplier_parameter: SupplierParameter
+
+    def setUp(self) -> None:
+        self.test_supplier = Supplier.objects.create(name=self.name,
+                                                     is_outdated=self.is_outdated)
+        self.test_supplier_parameter = SupplierParameter.objects.create(supplier=self.test_supplier,
+                                                                        parameter_name=self.parameter_name,
+                                                                        is_outdated=self.is_outdated)
+
+    def tearDown(self) -> None:
+        self.test_supplier.delete()
+        self.test_supplier_parameter.delete()
+
+    def test_creation(self):
+        self.assertEqual(str(self.test_supplier_parameter),
+                         f'{self.test_supplier_parameter.supplier.name} '
+                         f'\'{self.test_supplier_parameter.parameter_name}\' '
+                         f'({str(self.test_supplier_parameter.is_outdated)[0]})')
