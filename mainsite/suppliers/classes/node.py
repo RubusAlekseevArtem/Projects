ba8fa747c3f507bd2135e952fbfc8419e2023069
alekseev_a_practik <rubus_alekseev_a_pracktick@mail.ru>
2023-02-24 14:00:35 +0300

    add unique checking

--- mainsite/suppliers/classes/node.py
+++ mainsite/suppliers/classes/node.py
@@ -8,17 +8,22 @@ class NodeCreationError(Exception):
         super().__init__(self.__doc__)
 
 
+class UniqueNodeCreationError(Exception):
+    """Error creating a unique node"""
+
+    def __init__(self):
+        super().__init__(self.__doc__)
+
+
 class Node:
     NUMBER_KEY = 'Number'
     NAME_KEY = 'Name'
     CHILDREN_KEY = 'Children'
 
-    def __init__(self, number: str, name: str, parent=None):
-        self.parent: Node = parent
+    def __init__(self, number: str, name: str):
         self.number = number
         self.name = name
         self.children: List[Node] = []
-        # add unique number
 
     def __str__(self):
         return f'{self.number} {self.name} {self.children}'
@@ -28,28 +33,40 @@ class Node:
 
     def __eq__(self, other):
         if isinstance(other, Node):
-            return \
-                    self.parent == other.parent and \
-                    self.number == other.number and \
-                    self.name == other.name and \
-                    self.children == other.children
+            return self.number == other.number and \
+                self.name == other.name and \
+                self.children == other.children
         return False
-
-    def has_parent(self) -> bool:
-        return self.parent is not None
 
     def has_children(self) -> bool:
         return len(self.children) > 0
 
-    def add_child(self, node):
+    def _add_child(self, node):
         if not isinstance(node, Node):
             raise NodeCreationError()
         self.children.append(node)
         return self
 
-    def add_children(self, nodes: Iterable):
+    def add_children(self, nodes: Iterable, check_unique_numbers=True):
         for node in nodes:
-            self.add_child(node)
+            self._add_child(node)
+        if check_unique_numbers:
+            self.check_parents_numbers()
+        return self
+
+    def check_parents_numbers(self, unique_number=True):
+        def _get_parents_numbers(children, result_list=None):
+            if result_list is None:
+                result_list = []
+            for node in children:
+                if node.has_children():
+                    _get_parents_numbers(node.children, result_list)
+                if node.number in result_list and unique_number:
+                    raise UniqueNodeCreationError()  # tree should not have duplicate numbers
+                result_list.append(node.number)
+
+        if self.has_children():
+            _get_parents_numbers(self.children, [self.number])
         return self
 
     def create_hierarchical_tree(self):
@@ -99,3 +116,14 @@ class Node:
             return None
 
         return _find_child_node_by_name(self, name)
+
+
+if __name__ == '__main__':
+    main_node = Node('root', 'Материал DKC').add_children([
+        Node('general_params', 'Материал').add_children([
+            Node('general_params', 'Сертификат__')
+        ]),
+        Node('material_certificates', 'Сертификаты материала'),
+        Node('stock', 'Остатки на складах'),
+    ])
+    print(main_node)
