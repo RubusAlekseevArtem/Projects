bbc87dac3d82036d194f3a4f01b207d846d24cbc
alekseev_a_practik <rubus_alekseev_a_pracktick@mail.ru>
2023-03-10 10:44:00 +0300

    complete chint tree

--- mainsite/suppliers/classes/hierarchical_trees/chint_hierarchical_tree.py
+++ mainsite/suppliers/classes/hierarchical_trees/chint_hierarchical_tree.py
@@ -9,129 +9,108 @@ def get_chint_product(obj: dict):
 
 
 def get_general_parameters(obj: dict):
-    return get_chint_product(obj).get('material')
-
-
-def get_info(obj: dict):
-    general_parameters = get_general_parameters(obj)
-    if general_parameters is None:
-        return None
-    else:
-        return dict(islice(general_parameters.items(), 0, 15))  # slice first 15 keys and values
+    return dict(islice(get_chint_product(obj).items(), 0, 28))
 
 
 def get_photo(obj: dict):
     return dict(islice(get_general_parameters(obj).items(), 15, 17))
 
 
-def get_video(obj: dict):
-    return get_chint_product(obj).get('video')
+def get_files(obj: dict):
+    return get_chint_product(obj).get('files')
 
 
-def get_url_to_the_video(obj: dict):
-    return list(map(lambda i: i.get('url'), get_video(obj)))
+def get_url_to_document(obj: dict):
+    return list(map(lambda i: i.get('file'), get_files(obj)))
 
 
-def get_cover(obj: dict):
-    return list(map(lambda i: i.get('cover'), get_video(obj)))
+def get_document_name(obj: dict):
+    return list(map(lambda i: i.get('name'), get_files(obj)))
 
 
-def get_link_type(obj: dict):
-    return list(map(lambda i: i.get('type'), get_video(obj)))
+def get_document_date(obj: dict):
+    return list(map(lambda i: i.get('date'), get_files(obj)))
 
 
 class ChintHierarchicalTreeParameters(BaseHierarchicalTree):
     def __init__(self):
-        root_node = FuncNode('chint_product', 'Продукт E-CHINT API', get_chint_product).add_children(
+        root_node = FuncNode('chint_product', 'Продукт E-CHINT', get_chint_product).add_children(
             [
                 FuncNode(
-                    'general_parameters',
-                    'Главные параметры',
+                    'general_parameters', 'Главные параметры',
                     get_general_parameters).add_children(
                     [
-                        FuncNode('info', 'Информация по материалу', get_info).add_children(
-                            [
-                                FuncNode('id', 'id',
-                                         lambda o: get_info(o).get('id')),
-                                FuncNode('node_id', 'node_id',
-                                         lambda o: get_info(o).get('node_id')),
-                                FuncNode('etim_class_id', 'etim_class_id',
-                                         lambda o: get_info(o).get('etim_class_id')),
-                                FuncNode('name', 'Название',
-                                         lambda o: get_info(o).get('name')),
-                                FuncNode('type', 'Тип',
-                                         lambda o: get_info(o).get('type')),
-                                FuncNode('series', 'Серия',
-                                         lambda o: get_info(o).get('series')),
-                                FuncNode('country', 'Страна',
-                                         lambda o: get_info(o).get('country')),
-                                FuncNode('unit', 'Единица измерения',
-                                         lambda o: get_info(o).get('unit')),
-                                FuncNode('volume', 'Объем',
-                                         lambda o: get_info(o).get('volume')),
-                                FuncNode('weight', 'Вес',
-                                         lambda o: get_info(o).get('weight')),
-                                FuncNode('code', 'Код материала',
-                                         lambda o: get_info(o).get('code')),
-                                FuncNode('url', 'Ссылка на материал',
-                                         lambda o: get_info(o).get('url')),
-                                FuncNode('price', 'Цена',
-                                         lambda o: get_info(o).get('price')),
-                                FuncNode('no_price', 'Никакой цены?',
-                                         lambda o: get_info(o).get('no_price')),
-                                FuncNode('barcode', 'Штрих-код',
-                                         lambda o: get_info(o).get('barcode')),
-                            ],
-                        ),
-                        FuncNode('photo', 'Фото материала', get_photo).add_children(
-                            [
-                                FuncNode(
-                                    'thumbnail_url', 'Миниатюра материала',
-                                    lambda o: get_photo(o).get('thumbnail_url')),
-                                FuncNode(
-                                    'additional_images',
-                                    'Дополнительные изображения',
-                                    lambda o: get_photo(o).get('additional_images')),
-                            ],
-                        ),
-                        FuncNode('attributes', 'Атрибуты материала',
-                                 lambda o: get_general_parameters(o).get('attributes')),
-                        FuncNode('etim_attributes', 'ETIM атрибуты материала',
-                                 lambda o: get_general_parameters(o).get('etim_attributes')),
-                        FuncNode('packing', 'Фасовка',
-                                 lambda o: get_general_parameters(o).get('packing')),
-                        FuncNode('avg_delivery', 'Средняя доставка',
-                                 lambda o: get_general_parameters(o).get('avg_delivery')),
-                        FuncNode('accessories', 'Аксессуары',
-                                 lambda o: get_general_parameters(o).get('accessories')),
-                        FuncNode('accessories_codes', 'Коды аксессуаров',
-                                 lambda o: get_general_parameters(o).get('accessories_codes')),
-                        FuncNode('sale', 'Скидки на материал',
-                                 lambda o: get_general_parameters(o).get('sale')),
+                        FuncNode('id', 'id',
+                                 lambda o: get_general_parameters(o).get('id')),
+                        FuncNode('parent_id', 'parent_id',
+                                 lambda o: get_general_parameters(o).get('parent_id')),
+                        FuncNode('full_name', 'full_name',
+                                 lambda o: get_general_parameters(o).get('full_name')),
+                        FuncNode('short_name', 'short_name',
+                                 lambda o: get_general_parameters(o).get('short_name')),
+                        FuncNode('name', 'name',
+                                 lambda o: get_general_parameters(o).get('name')),
+                        FuncNode('vendor_code', 'vendor_code',
+                                 lambda o: get_general_parameters(o).get('vendor_code')),
+                        FuncNode('b2b_illiquid', 'b2b_illiquid',
+                                 lambda o: get_general_parameters(o).get('b2b_illiquid')),
+                        FuncNode('b2b_new_аrticle', 'b2b_new_аrticle',
+                                 lambda o: get_general_parameters(o).get('b2b_new_аrticle')),
+                        FuncNode('out_of_price', 'out_of_price',
+                                 lambda o: get_general_parameters(o).get('out_of_price')),
+                        FuncNode('status_article', 'status_article',
+                                 lambda o: get_general_parameters(o).get('status_article')),
+                        FuncNode('pack_indivisibility', 'pack_indivisibility',
+                                 lambda o: get_general_parameters(o).get('pack_indivisibility')),
+                        FuncNode('unit_name', 'unit_name',
+                                 lambda o: get_general_parameters(o).get('unit_name')),
+                        FuncNode('unit_id', 'unit_id',
+                                 lambda o: get_general_parameters(o).get('unit_id')),
+                        FuncNode('brutto', 'brutto',
+                                 lambda o: get_general_parameters(o).get('brutto')),
+                        FuncNode('weight', 'weight',
+                                 lambda o: get_general_parameters(o).get('weight')),
+                        FuncNode('volume', 'volume',
+                                 lambda o: get_general_parameters(o).get('volume')),
+                        FuncNode('group_pack_name', 'group_pack_name',
+                                 lambda o: get_general_parameters(o).get('group_pack_name')),
+                        FuncNode('group_pack_id', 'group_pack_id',
+                                 lambda o: get_general_parameters(o).get('group_pack_id')),
+                        FuncNode('group_pack_brutto', 'group_pack_brutto',
+                                 lambda o: get_general_parameters(o).get('group_pack_brutto')),
+                        FuncNode('group_pack_weight', 'group_pack_weight',
+                                 lambda o: get_general_parameters(o).get('group_pack_weight')),
+                        FuncNode('group_pack_volume', 'group_pack_volume',
+                                 lambda o: get_general_parameters(o).get('group_pack_volume')),
+                        FuncNode('b2b', 'b2b',
+                                 lambda o: get_general_parameters(o).get('b2b')),
+                        FuncNode('cert', 'cert',
+                                 lambda o: get_general_parameters(o).get('cert')),
+                        FuncNode('price_group', 'price_group',
+                                 lambda o: get_general_parameters(o).get('price_group')),
+                        FuncNode('etim', 'etim',
+                                 lambda o: get_general_parameters(o).get('etim')),
+                        FuncNode('min', 'min',
+                                 lambda o: get_general_parameters(o).get('min')),
+                        FuncNode('pv', 'pv',
+                                 lambda o: get_general_parameters(o).get('pv')),
+                        FuncNode('avia', 'avia',
+                                 lambda o: get_general_parameters(o).get('avia')),
                     ]
                 ),
-                FuncNode('certificates', 'Сертификаты',
-                         lambda o: get_chint_product(o).get('certificates')),
-                FuncNode('stock', 'Остатки на складах',
-                         lambda o: get_chint_product(o).get('stock')),
-                FuncNode('related', 'Сопутствующие материалы',
-                         lambda o: get_chint_product(o).get('related')),
-                FuncNode('video', 'Видео', get_video).add_children(
-                    [
-                        FuncNode('url_to_the_video', 'Ссылка на видео',
-                                 get_url_to_the_video),
-                        FuncNode('cover', 'Ссылка на обложку видео', get_cover),
-                        FuncNode('link_type', 'Тип ссылки видео', get_link_type),
-                    ]
-                ),
-                FuncNode('drawings_sketch', 'Эскизы чертежей',
-                         lambda o: get_chint_product(o).get('drawings_sketch')),
-                FuncNode('description', 'Описание',
-                         lambda o: get_chint_product(o).get('description')),
-                FuncNode('analogs', 'Аналоги',
-                         lambda o: get_chint_product(o).get('analogs')),
-                FuncNode('specification', 'Пересчет спецификации',
-                         lambda o: get_chint_product(o).get('specification')),
+                FuncNode('properties', 'Свойства (тех характеристики)',
+                         lambda o: get_chint_product(o).get('properties')),
+                FuncNode('picture', 'Фото',
+                         lambda o: get_chint_product(o).get('picture')),
+                FuncNode('documents', 'Документы', get_files)
+                # .add_children(
+                #     [
+                #         FuncNode('url_to_document', 'Ссылка на документ',
+                #                  get_url_to_document),
+                #         FuncNode('document_name', 'Наименование документа', get_document_name),
+                #         FuncNode('document_date', 'Дата действительности документа', get_document_date),
+                #     ]
+                # ),
             ]
         )
         super().__init__(root_node)
