381f61325850f58ff3896dcd09ca4a6f8c177399
alekseev_a_practik <rubus_alekseev_a_pracktick@mail.ru>
2023-03-10 10:54:12 +0300

    reformat file

--- DKC_API/main.py
+++ DKC_API/main.py
@@ -1,10 +1,11 @@
 import json
 import pprint
 
-from DKC_API.dkc_catalog_material import DkcObj
-from DKC_API.private_file import HEADERS, INDENT
+from DKC_API.private_file import INDENT
+from DKC_API.dkc_catalog_material import get_dkc_materials
 
 PRINT_TO_CONSOLE = False
+ENCODING = 'utf-8'
 
 
 def my_print(data):
@@ -12,41 +13,24 @@ def my_print(data):
         pprint.pprint(data, indent=2)
 
 
-def save_to_file(dkc, materials, filename):
+def save_to_file(materials, filename):
     json_data = json.dumps(
         [dict(material) for material in materials],
         indent=INDENT,
         ensure_ascii=False
     )
-    with open(filename, mode='w', encoding=dkc.ENCODING) as f:
+    with open(filename, mode='w', encoding=ENCODING) as f:
         f.writelines(json_data)
 
 
-def get_materials(material_codes, dkc=None):
-    if dkc is None:
-        # try:
-        dkc = DkcObj()
-        return dkc.get_materials(material_codes)
-        # except DkcAccessTokenError as err:
-        #     my_print(err)
-    else:
-        return dkc.get_materials(material_codes)
-
-
-def main():
-    dkc = DkcObj()
-    if dkc.access_token:
-        HEADERS['AccessToken'] = dkc.access_token
-
-        material_codes = ['4400003']
-        materials = get_materials(material_codes, dkc)
-        if materials:
-            my_print(materials)
-            print(materials)
-        save_to_file(dkc, materials, 'load_data.txt')
-    else:
-        print('Ошибка получения токена для доступа')
+def get_materials(material_codes):
+    return get_dkc_materials(material_codes)
 
 
 if __name__ == '__main__':
-    main()
+    material_codes = ['4400003']
+    materials = get_dkc_materials(material_codes)
+    if materials:
+        my_print(materials)
+        print(materials)
+    save_to_file(materials, 'load_data.txt')
